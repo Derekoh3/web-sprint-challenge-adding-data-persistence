ef415b4e77a41eec8facd5eaac8a84d7d2397a3f
Derek Oh <derek.oh17@gmail.com>
2021-10-08 15:15:39 -0400

    solved mvp

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,25 @@
 // build your `Resource` model here
+const express = require("express");
+const Project = require("./model");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const projects = await Project.getAll();
+    res.status(200).json(projects);
+  } catch (err) {
+    next(nerr);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const response = await Project.insert(req.body);
+    res.status(201).json(response);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
